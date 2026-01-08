b2536475b43bc41299ad107d8d0b2f415de54690
maksimmishakov <maksmisakov@gmail.com>
2026-01-08 15:12:11 +0000

    fix: correct bcrypt context initialization with truncate_error parameter

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -2,8 +2,7 @@ from app import db
 from datetime import datetime
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="atruncate_error=False)
-
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", truncate_error=False)
 class User(db.Model):
     __tablename__ = 'users'
     
