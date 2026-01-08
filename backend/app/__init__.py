b607ac1dab63cf7b50943ca6ccaca74a68758d3c
maksimmishakov <maksmisakov@gmail.com>
2026-01-08 14:26:46 +0000

    fix: resolve circular imports with database.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from .database import db
 
 db = SQLAlchemy()
 jwt = JWTManager()
