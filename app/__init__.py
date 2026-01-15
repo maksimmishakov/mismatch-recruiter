77aa529f605a6281aeed233061906f2e2a7b659d
maksimmishakov <maksmisakov@gmail.com>
2026-01-15 18:13:27 +0300

    Fix missing app.config[ on line 19

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app(config=None):
     # Config
     from app.config import MismatchSettings, get_settings
     settings = get_settings()
-    SQLALCHEMY_DATABASE_URI'] = settings.Mismatch_db_connection or 'sqlite:////tmp/mismatch.db'
+app.config['    SQLALCHEMY_DATABASE_URI'] = settings.Mismatch_db_connection or 'sqlite:////tmp/mismatch.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     if config:
