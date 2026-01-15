d79a0c19e23fab38708c0885e10fc60b57757129
maksimmishakov <maksmisakov@gmail.com>
2026-01-15 15:59:02 +0300

    Fix circular import - remove Config import from app.config package

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -3,9 +3,6 @@ from app.config.mismatch import (
     MismatchSettings,    get_settings,
     get_mismatch_config,
 )
-# Import Config from parent config module
-from app import config as config_module
-Config = config_module.Config
 
 __all__ = [
     "MismatchSettings",
