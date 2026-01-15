43f4327336e968a4fa8708390b21ae14bda81c0e
maksimmishakov <maksmisakov@gmail.com>
2026-01-15 15:43:42 +0300

    Clean config __init__ - remove non-existent MISMATCH constants imports

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -3,24 +3,10 @@ from app.config.mismatch import (
     MismatchSettings,
     get_settings,
     get_mismatch_config,
-    MISMATCH_JOB_STATUS_OPTIONS,
-    MISMATCH_EXPERIENCE_LEVELS,
-    MISMATCH_EMPLOYMENT_TYPES,
-    MISMATCH_PLACEMENT_STATUSES,
-    MISMATCH_SYNC_TYPES,
-    MATCHING_WEIGHTS,
-    MISMATCH_ERROR_CODES,
 )
 
 __all__ = [
     "MismatchSettings",
     "get_settings",
     "get_mismatch_config",
-    "MISMATCH_JOB_STATUS_OPTIONS",
-    "MISMATCH_EXPERIENCE_LEVELS",
-    "MISMATCH_EMPLOYMENT_TYPES",
-    "MISMATCH_PLACEMENT_STATUSES",
-    "MISMATCH_SYNC_TYPES",
-    "MATCHING_WEIGHTS",
-    "MISMATCH_ERROR_CODES",
 ]
