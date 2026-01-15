decbbada95291155a4a66ed11b77cd8f06448d41
maksimmishakov <maksmisakov@gmail.com>
2026-01-15 15:53:59 +0300

    Fix Config import - handle app.config module/package naming collision

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,14 +1,16 @@
-"""Configuration module."""
+"""Configuration package."""
 from app.config.mismatch import (
-    MismatchSettings,,
-    Config
+    MismatchSettings,
     get_settings,
     get_mismatch_config,
 )
+# Import Config from parent config module
+from app import config as config_module
+Config = config_module.Config
 
 __all__ = [
     "MismatchSettings",
     "get_settings",
     "get_mismatch_config",
-        "Config",
+    "Config",
 ]
