c5efdf4ef13ef8a06675713bd8f337b4204cc518
maksimmishakov <maksmisakov@gmail.com>
2026-01-16 17:03:34 +0300

    CRITICAL FIX: Unify PORT to 5000 in main.py

--- main.py
+++ main.py
@@ -111,4 +111,4 @@ def health():
     return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
