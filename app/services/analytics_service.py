b82d67e016c2d8aece5904dae421a7199a39ab38
maksimmishakov <maksmisakov@gmail.com>
2026-01-16 19:39:46 +0300

    Fix: Properly format import lines in analytics_service.py

--- app/services/analytics_service.py
+++ app/services/analytics_service.py
@@ -8,7 +8,8 @@ from datetime import datetime, timedelta
 from typing import Dict, List, Optional
 from sqlalchemy import func
 from app.database import SessionLocal
-# from app.models import Resume, Job, Match  # TODO: These classes need to be definedimport logging
+# from app.models import Resume, Job, Match  # TODO: These classes need to be defi
+import logging
 
 logger = logging.getLogger(__name__)
 
